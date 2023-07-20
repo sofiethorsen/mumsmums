ff98f485ca484fc9dcaadc25f6fa4dd479575a56
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2023-07-20 15:49:38 +0200

    Add ktor webserver

---
+++ src/server/jvmMain/kotlin/app/mumsmums/plugins/Routing.kt
@@ -0,0 +1,20 @@
+package app.mumsmums.plugins
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.auth.UserIdPrincipal
+import io.ktor.server.auth.authenticate
+import io.ktor.server.auth.principal
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+
+fun Application.configureRouting() {
+    routing {
+        authenticate("auth-basic") {
+            get("/") {
+                call.respondText("Hello, ${call.principal<UserIdPrincipal>()?.name}!")
+            }
+        }
+    }
+}

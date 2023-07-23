2edc977953187bd4c658e81ee267785715895a94
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2023-07-23 15:08:47 +0200

    Setup workflow for client code

---
+++ ci/pr-build-client.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+GIT_DIR=$(git rev-parse --show-toplevel)
+cd "$GIT_DIR/src/client" || exit 1
+
+npm ci
+npm run lint
+npm run build --if-present
+npm test
+
+cd "$GIT_DIR" || exit 1

512f9bf9a701b9f4f86c9f172cd88a206b2d2e9b
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2023-07-28 22:18:47 +0200

    Use webpack to check if dev or prod (#12)

---
+++ src/client/webpack.dev.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    port: '3000',
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+    liveReload: true,
+  },
+})

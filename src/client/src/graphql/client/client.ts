e8bc702fadfe1ebca7683bb05d72446407eeab3c
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2023-07-28 16:34:30 +0200

    Update comment

--- src/client/src/graphql/client/client.ts
+++ src/client/src/graphql/client/client.ts
@@ -1,7 +1,7 @@
 import fetch from 'cross-fetch';
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
-// TODO: set via webpack config
+// TODO: set via webpack config, i.e. something like:
 //const host = process.env.NODE_ENV === "development" ? "http://localhost:8080" : ""
 const isProd = true
 const host = isProd ? "" : "http://localhost:8080"

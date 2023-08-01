e58f89408375c3fd9070c54f9e9edd112f1bdf0c
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2023-08-01 21:37:08 +0200

    Add number generator utility for ID generation (#17)
    * Add number generator utility for ID generation
    
    * Setup bazel unit test
    
    * Update CI script
    
    * Inline
    
    * Update comment

---
+++ bazel/junit5/defs.bzl
@@ -0,0 +1,85 @@
+load("@io_bazel_rules_kotlin//kotlin:jvm.bzl", "kt_jvm_library", "kt_jvm_test")
+
+def kt_jvm_junit5_test(name, srcs = [], test_package = None, deps = [], runtime_deps = [], **kwargs):
+    """Generates kotlin test targets that run using junit 5"""
+
+    # args to ignore
+    FILTER_KWARGS = [
+        "main_class",
+        "args",
+    ]
+
+    for arg in FILTER_KWARGS:
+        if arg in kwargs.keys():
+            kwargs.pop(arg)
+
+    if not test_package:
+        fail("must specify test package")
+
+    if not srcs:
+        fail("must specify sources")
+
+    # Add all the test deps here instead of per test bazel file
+    test_deps = [
+        "@maven//:org_junit_jupiter_junit_jupiter_api",
+        "@maven//:org_junit_jupiter_junit_jupiter_engine",
+        "@maven//:org_junit_jupiter_junit_jupiter_params",
+        "@maven//:org_junit_platform_junit_platform_suite_api",
+    ]
+    test_runtime_deps = [
+        "@maven//:org_junit_platform_junit_platform_commons",
+        "@maven//:org_junit_platform_junit_platform_console",
+        "@maven//:org_junit_platform_junit_platform_engine",
+        "@maven//:org_junit_platform_junit_platform_launcher",
+        "@maven//:org_junit_platform_junit_platform_suite_api",
+    ]
+
+    # If we dynamically generate tests with new names, create a suite with the name provided by the user
+    # so they can still run all tests using the target in their build file.
+    test_suite_tests = []
+
+    # Generate a test rule for each class
+    for src in srcs:
+        classname = src[:src.rfind(".")]  # strip '.<extension>'
+        _rule_kt_test(
+            srcs = [src],
+            name = classname,
+            deps = depset(deps + test_deps).to_list(),
+            test_package = test_package,
+            runtime_deps = depset(runtime_deps + test_runtime_deps).to_list(),
+            **kwargs
+        )
+        if name != classname:
+            test_suite_tests.append(classname)
+
+    if test_suite_tests:
+        native.test_suite(
+            name = name,
+            tags = kwargs.get("tags", []),
+            tests = test_suite_tests,
+            visibility = ["//:__subpackages__"],
+        )
+
+def _rule_kt_test(name, test_package, srcs = [], runtime_deps = [], **kwargs):
+    """Helper macro for calling kt_jvm_test"""
+    args = [
+        "--disable-banner",
+        "--include-classname=.*",
+    ]
+
+    if "/" in name:
+        args.append("--select-package")
+        args.append(test_package)
+    else:
+        args.append("--select-class")
+        args.append(test_package + "." + name[name.rfind("/") + 1:])
+
+    kt_jvm_test(
+        name = name,
+        srcs = srcs,
+        testonly = True,
+        main_class = "org.junit.platform.console.ConsoleLauncher",
+        args = args,
+        runtime_deps = runtime_deps,
+        **kwargs
+    )
---
+++ src/server/jvmMain/kotlin/app/mumsmums/identifiers/NumericIdGenerator.kt
@@ -0,0 +1,104 @@
+package app.mumsmums.identifiers
+
+import java.net.InetAddress
+
+import com.amazonaws.util.EC2MetadataUtils
+
+/**
+ * Use 12 bits for the machine ID. When running on EC2, use the EC2 instance IP address lower 12 bits. This is
+ * guaranteed to be unique as they are in a network with a 20 CIDR mask size, leaving 12 bits for the host address.
+ */
+private const val bitsInMachineId = 12
+
+/**
+ * Use 29 bits for timestamp. It has a custom epoch (01/08/2023 @ 00:00 UTC) and second-precision.
+ */
+private const val bitsInTimestamp = 29
+
+/**
+ * Use 11 bits for counter. This would be able to generate 2048 ids per second, per machine. This is more than
+ * enough for a hobby project. If to be used at a larger scale than that, we'd have to introduce a synchronous sleep
+ * or similar, to force uniqueness.
+ */
+private const val bitsInCounter = 11
+
+// Tuesday, 1 August 2023 00:00:00 UTC
+const val epochOffset: Long = 1690848000
+
+// Get the IP address of the machine, which is used for generating the numeric ID.
+private fun getIpAddress(): InetAddress {
+    return if (System.getProperty("os.name").startsWith("Mac")) InetAddress.getLocalHost()
+    else InetAddress.getByName(EC2MetadataUtils.getPrivateIpAddress())
+}
+
+/**
+ * Generates numeric IDs that combines the machine ID (derived from the IP address), a timestamp, and a counter. This
+ * combination ensures that the generated numeric IDs are unique and monotonically increasing even when generated on
+ * multiple machines concurrently (see above comments though).
+ *
+ * The numeric IDs are longs (52 bits) which is less than a UUID (128 bits) and makes for faster indexing and simpler
+ * comparisons.
+ */
+class NumericIdGenerator(
+        private val ipAddress: InetAddress = getIpAddress(),
+        private val currentTimestampMillis: () -> Long = { System.currentTimeMillis() },
+        private val counterStart: Int = 0,
+) {
+    // Get the raw IP address in the in network byte order, highest order byte is in the first octet.
+    private val ipAddressBytes = ipAddress.address
+
+    // Convert the raw IPv4 address into a single 32-bit unsigned integer (i.e. Long in Kotlin) as the machine ID.
+    // We do this by applying a bitmask on each octet, to ensure we get a positive value between 0 and 255.
+    //
+    // For an example, consider an IP address like 192.168.1.10.
+    //
+    // - The ipAddressBytes array would look like [192, 168, 1, 10].
+    // - The first octet (ipAddressBytes[0]) is 192 (0xC0 in hexadecimal).
+    // - The second octet (ipAddressBytes[1]) is 168 (0xA8 in hexadecimal).
+    // - The third octet (ipAddressBytes[2]) is 1 (0x01 in hexadecimal).
+    // - The fourth octet (ipAddressBytes[3]) is 10 (0x0A in hexadecimal).
+    //
+    // - ipAddressBytes[0].toLong() and 0xFFL is 192 and 0xFFL, which gives 192.
+    // - 192 shl (3 * 8) shifts 192 to the left by 24 bits, giving 3221225472.
+    // - ipAddressBytes[1].toLong() and 0xFFL is 168 and 0xFFL, which gives 168.
+    // - 168 shl (2 * 8) shifts 168 to the left by 16 bits, giving 1101004800.
+    // - ipAddressBytes[2].toLong() and 0xFFL is 1 and 0xFFL, which gives 1.
+    // - 1 shl (1 * 8) shifts 1 to the left by 8 bits, giving 256.
+    // - ipAddressBytes[3].toLong() and 0xFFL is 10 and 0xFFL, which gives 10.
+    //
+    // Together this results in the ID: 3221225472 + 1101004800 + 256 + 10 = 4328719366
+    private val ipAddressAsLong: Long =
+            ((ipAddressBytes[0].toLong() and 0xFFL) shl (3 * 8)) +
+                    ((ipAddressBytes[1].toLong() and 0xFFL) shl (2 * 8)) +
+                    ((ipAddressBytes[2].toLong() and 0xFFL) shl (1 * 8)) +
+                    (ipAddressBytes[3].toLong() and 0xFFL)
+
+    // Calculate the machine ID bits by combining the IP address with timestamp and counter bits.
+    private val machineIdBit = (ipAddressAsLong and ((1L shl bitsInMachineId) - 1)) shl
+            (bitsInTimestamp + bitsInCounter)
+
+    // Initialize the last timestamp and the counter for generating numeric IDs.
+    private var lastTimestamp: Long = currentTimestampMillis() / 1000L - epochOffset
+    private var counter = counterStart
+
+    // Generate a new numeric ID based on the current timestamp and the machine ID.
+    fun generateId(): Long {
+        val currentTimestamp = currentTimestampMillis() / 1000L - epochOffset
+
+        // If the timestamp has advanced, reset the counter.
+        if (currentTimestamp > lastTimestamp) {
+            lastTimestamp = currentTimestamp
+            counter = 0
+        }
+
+        val counterToReturn = counter
+        counter += 1
+
+        // Combine machine ID, timestamp, and counter bits to create a unique numeric ID.
+        val timestampBit = (lastTimestamp and ((1L shl bitsInTimestamp) - 1)) shl bitsInCounter
+        val counterBit = (counterToReturn and ((1 shl bitsInCounter) - 1)).toLong()
+
+        // lastly mask the 52 bits
+        return (machineIdBit or timestampBit or counterBit) and 0x000FFFFFFFFFFFFFL
+    }
+}

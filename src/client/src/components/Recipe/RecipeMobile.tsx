10a21fc0d655a717b7de74ce52ca33124c45f4b2
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2023-08-04 16:12:14 +0200

    Extract IngredientsCard component

--- src/client/src/components/Recipe/RecipeMobile.tsx
+++ src/client/src/components/Recipe/RecipeMobile.tsx
@@ -2,56 +2,16 @@ import React from 'react'
 import styles from './RecipeMobile.module.css'
 
 import ErrorMessage from '../ErrorMessage/ErrorMessage'
-import { Ingredient, IngredientSection } from '../../graphql/client/types'
+
 import { useQuery } from '@apollo/client'
 
 import SquareRecipeImage from '../SquareRecipeImage/SquareRecipeImage'
+import IngredientsCard from '../IngredientsCard/IngredientsCard'
 
 import { GET_RECIPE_BY_ID } from './queries'
 
 interface RecipeProps {
     recipeId: number
-}
-
-const renderServingsOrUnitsInfo = (servings: number | undefined, numberOfUnits: number | undefined) => {
-    const info = (numberOfUnits && `${numberOfUnits} st`) || `${servings} port.`
-
-    return (
-        <div className={styles.servings}>
-            {info}
-        </div>
-    )
-}
-
-const renderSectionTitle = (name: string | undefined) => {
-    if (name) {
-        return <div className={styles.sectionTitle}>{name}</div>
-    } else {
-        return null
-    }
-}
-
-const renderIngredient = (ingredient: Ingredient, index: number) => {
-    const quantity = ingredient.quantity && `${ingredient.quantity} `
-    const volume = ingredient.volume && `${ingredient.volume} `
-
-    return (
-        <div className={styles.ingredient} key={`ingredient-${index}`}>{quantity}{volume}{ingredient.name}</div>
-    )
-}
-
-const renderIngredientSection = (section: IngredientSection, sectionIndex: number) => {
-    return (
-        <div key={`section-${sectionIndex}`} className={styles.section}>
-            {renderSectionTitle(section.name)}
-            <div className={styles.ingredients}>
-                {section.ingredients.map((ingredient: Ingredient, index: number) => (
-                    renderIngredient(ingredient, index)
-                ))
-                }
-            </div>
-        </div>
-    )
 }
 
 const RecipeMobile: React.FC<RecipeProps> = ({ recipeId }) => {
@@ -75,11 +35,7 @@ const RecipeMobile: React.FC<RecipeProps> = ({ recipeId }) => {
                 <div>
                     <SquareRecipeImage imageUrl={recipe.imageUrl} imageAltText={Image.name} />
                     <div className={styles.ingredients}>
-                        <div className={styles.ingredientsCard}>
-                            <div className={styles.title}>Ingredienser</div>
-                            {renderServingsOrUnitsInfo(recipe.servings, recipe.numberOfUnits)}
-                            {recipe.ingredientSections.map((section: IngredientSection, index: number) => renderIngredientSection(section, index))}
-                        </div>
+                        <IngredientsCard recipe={recipe} />
                     </div>
                 </div>
                 <div className={styles.instructions}>

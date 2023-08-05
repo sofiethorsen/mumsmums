9b2bb6a9d2be3a1fc61d68a0fe8c9581d88be3d2
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2023-08-05 15:53:38 +0200

    Build deploy jar

--- ci/pr-build-server.sh
+++ ci/pr-build-server.sh
@@ -5,5 +5,9 @@ set -euo pipefail
 GIT_DIR=$(git rev-parse --show-toplevel)
 cd "$GIT_DIR" || exit 1
 
+# Build and test everything
 bazel build //...
 bazel test //...
+
+# Build the deploy jar
+bazel build //src/server/jvmMain/kotlin/app/mumsmums:mumsmums_deploy.jar

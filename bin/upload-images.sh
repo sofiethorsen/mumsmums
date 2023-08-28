d83e05d7eecbeb682e3e630e9d9cbd88c7fa6ad2
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2023-08-28 20:42:36 +0200

    Update stack to setup s3 and cf

---
+++ bin/upload-images.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+BUCKET_NAME="mumsmums"
+
+# Dir for the images we're uploading
+BASE_DIR="/Users/sthorsen/Downloads/mumsimg"
+RECIPE_ID="43982676217857"
+
+# Loop through image files in the base directory
+for FILE_PATH in "$BASE_DIR"/*; do
+    # Extract the filename without the path
+    FILENAME=$(basename "$FILE_PATH")
+
+    # Upload the image to S3 with appropriate prefixes
+    aws s3 cp "$FILE_PATH" "s3://$BUCKET_NAME/assets/$RECIPE_ID/$FILENAME"
+done

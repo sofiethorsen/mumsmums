3cb5a6f065e9486cdd498dc731003a95b71ec3e3
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2023-08-29 12:15:52 +0200

    Revert lazy loading

--- src/client/src/components/SquareRecipeImage/SquareRecipeImage.tsx
+++ src/client/src/components/SquareRecipeImage/SquareRecipeImage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import styles from './SquareRecipeImage.module.css'
 import generateHexColor from './colorGenerator'
 
@@ -12,9 +12,7 @@ const renderImage = (imageUrl: string, imageAltText: string) => {
     return <img
         src={imageUrl}
         alt={imageAltText}
-        data-src=""
         className={styles.squareImage}
-        loading="lazy"
     />
 }
 
@@ -26,41 +24,6 @@ const renderPlaceHolder = (recipeId: number) => {
 }
 
 const SquareRecipeImage: React.FC<SquareImageProps> = ({ imageUrl, imageAltText, recipeId }) => {
-    useEffect(() => {
-        const lazyImages = document.querySelectorAll(`.${styles.squareImage}`)
-
-        const observerOptions = {
-            root: null,
-            rootMargin: '0px',
-            threshold: 0.1, // 10% of the image visible
-        }
-
-        function handleIntersection(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    const img = entry.target
-                    img.setAttribute('data-src', img.getAttribute('src') || '')
-                    img.classList.add(styles.loaded)
-                    observer.unobserve(img) // Stop observing once image is loaded
-                }
-            })
-        }
-
-        const imageObserver = new IntersectionObserver(handleIntersection, observerOptions)
-
-        // Start observing each lazy image
-        lazyImages.forEach(image => {
-            imageObserver.observe(image)
-        })
-
-        return () => {
-            // Clean up the observer when the component unmounts
-            lazyImages.forEach(image => {
-                imageObserver.unobserve(image)
-            })
-        }
-    }, []) // Run only once on component mount
-
     return (
         <div className={styles.imageContainer}>
             {imageUrl && imageUrl.length > 0 && renderImage(imageUrl, imageAltText)}

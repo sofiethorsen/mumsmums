455dd6341b66cdddd6364cbcdf3a9ce7179fc781
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2023-09-03 18:33:13 +0200

    Set up basic link preview metadata

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 
 import ErrorPage from './src/views/ErrorPage/ErrorPage.tsx'
 import HomePage from './src/views/HomePage/HomePage.tsx'
@@ -26,6 +27,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   </React.StrictMode>,
 )
--- src/client/src/components/Recipe/RecipeMobile.tsx
+++ src/client/src/components/Recipe/RecipeMobile.tsx
@@ -9,6 +9,7 @@ import SquareRecipeImage from '../SquareRecipeImage/SquareRecipeImage'
 import IngredientsCard from '../IngredientsCard/IngredientsCard'
 
 import { GET_RECIPE_BY_ID } from './queries'
+import { Helmet } from 'react-helmet-async'
 
 interface RecipeProps {
     recipeId: number
@@ -29,33 +30,43 @@ const RecipeMobile: React.FC<RecipeProps> = ({ recipeId }) => {
     }
 
     return (
-        <div className={styles.container}>
-            <div className={styles.recipeName}>{recipe.name}</div>
-            <div className={styles.details}>
-                <div>
-                    <SquareRecipeImage
-                        imageUrl={recipe.imageUrl}
-                        imageAltText={recipe.name} 
-                        recipeId={recipe.recipeId}
-                    />
-                    <div className={styles.ingredients}>
-                        <IngredientsCard recipe={recipe} />
+        <>
+            <Helmet>
+                <title>mumsmums - {recipe.name}</title>
+                <meta property="og:title" content={recipe.name} />
+                <meta property="og:site_name" content="mumsmums" />
+                <meta property="og:type" content="article" />
+                <meta property="og:url" content={`https://mumsmums.app/recipe/${recipe.recipeId}`} />
+                {recipe.imageUrl && <meta property="og:image" content={recipe.imageUrl} />}
+            </Helmet>
+            <div className={styles.container}>
+                <div className={styles.recipeName}>{recipe.name}</div>
+                <div className={styles.details}>
+                    <div>
+                        <SquareRecipeImage
+                            imageUrl={recipe.imageUrl}
+                            imageAltText={recipe.name}
+                            recipeId={recipe.recipeId}
+                        />
+                        <div className={styles.ingredients}>
+                            <IngredientsCard recipe={recipe} />
+                        </div>
                     </div>
-                </div>
-                <div className={styles.instructions}>
-                    <div className={styles.instructionsCard}>
-                        <div className={styles.title}>Gör så här</div>
-                        <div className={styles.steps}>
-                            <ol>
-                                {recipe.steps.map((step: string, index: number) => (
-                                    <li key={`step-${index}`}>{step}</li>)
-                                )}
-                            </ol>
+                    <div className={styles.instructions}>
+                        <div className={styles.instructionsCard}>
+                            <div className={styles.title}>Gör så här</div>
+                            <div className={styles.steps}>
+                                <ol>
+                                    {recipe.steps.map((step: string, index: number) => (
+                                        <li key={`step-${index}`}>{step}</li>)
+                                    )}
+                                </ol>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 

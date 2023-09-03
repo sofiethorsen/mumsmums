58f214df3e6ae31454ff6bc847aca2cb82cafdcb
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2023-09-03 12:00:07 +0200

    Set cache-control on image upload

--- bin/upload-images.sh
+++ bin/upload-images.sh
@@ -6,7 +6,7 @@ BUCKET_NAME="mumsmums"
 
 # Dir for the images we're uploading
 BASE_DIR="/Users/sthorsen/Downloads/mumsimg"
-RECIPE_ID="43985745104896"
+RECIPE_ID="43986376914944"
 
 # Loop through image files in the base directory
 for FILE_PATH in "$BASE_DIR"/*; do
@@ -14,5 +14,5 @@ for FILE_PATH in "$BASE_DIR"/*; do
     FILENAME=$(basename "$FILE_PATH")
 
     # Upload the image to S3 with appropriate prefixes
-    aws s3 cp "$FILE_PATH" "s3://$BUCKET_NAME/assets/$RECIPE_ID/$FILENAME"
+    aws s3 cp "$FILE_PATH" "s3://$BUCKET_NAME/assets/$RECIPE_ID/$FILENAME" --metadata "Cache-Control=max-age=31536000"
 done

e79abf38674d8ab156bf70717dd85a31b655831a
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2023-09-10 22:16:23 +0200

    Use getStaticPaths and getStaticProps

--- src/client/pages/recipe/[recipeId].tsx
+++ src/client/pages/recipe/[recipeId].tsx
@@ -3,6 +3,18 @@ import RecipePage from '../../page-components/RecipePage/RecipePage'
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage'
 import { useRouter } from 'next/router'
 import PageFrame from '../../components/PageFrame/PageFrame'
+import { useQuery, gql } from '@apollo/client'
+import client from '../../graphql/client'
+
+import { GET_RECIPE_BY_ID } from '../../components/Recipe/queries'
+
+const GET_RECIPE_IDS = gql`
+  query {
+    recipes {
+      recipeId,
+    }
+  }
+`
 
 export default function Recipe() {
     const router = useRouter()
@@ -17,4 +29,32 @@ export default function Recipe() {
             <RecipePage recipeId={parsedRecipeId} />
         </PageFrame>
     )
+}
+
+export async function getStaticPaths() {
+    const { data } = await client.query({
+        query: GET_RECIPE_IDS,
+    })
+
+    const paths = data.recipes.map((recipe) => ({
+        params: { recipeId: String(recipe.recipeId) },
+    }))
+
+    return {
+        paths: paths,
+        fallback: false,
+    }
+}
+
+export async function getStaticProps({ params }) {
+    const recipeData = await client.query({
+        query: GET_RECIPE_BY_ID,
+        variables: { recipeId: params.recipeId },
+    })
+
+    return {
+        props: {
+            recipeData,
+        },
+    }
 }

ec38117bf3832430773c7cf2447b300d768c6ac5
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2023-09-17 15:17:46 +0200

    Add description and fbPreviewImageUrl properties to Recipe

--- src/scripts/jvmMain/kotlin/app/mumsmums/UpdateRecipeItem.kt
+++ src/scripts/jvmMain/kotlin/app/mumsmums/UpdateRecipeItem.kt
@@ -5,14 +5,14 @@ import app.mumsmums.db.RecipeTable
 
 private val amazonDynamoDb = DynamoClientFactory.getDynamoDbForScriptContext()
 private val table = RecipeTable(amazonDynamoDb)
-private val recipeId = 43982566760448L
+private val recipeId = 43982676217857L
 
 fun main() {
     table.get(recipeId)?.let { original ->
         println(original)
 
-        val updated = original.copy(name = "Test")
-        val result = table.update(recipeId, updated, setOf("name"))
+        val updated = original.copy(fbPreviewImageUrl = "https://dmdqeeh0foqsn.cloudfront.net/assets/43982676217857/1200-600.webp")
+        val result = table.update(recipeId, updated, setOf("fbPreviewImageUrl"))
 
         println(result)
     }

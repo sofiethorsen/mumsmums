48ad254f3cf37340c20e89de322d9ca9e76c9b0b
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2023-09-17 15:22:15 +0200

    Update metatags to use description and fbPreviewImageUrl if available

--- src/client/page-components/RecipePage/RecipePage.tsx
+++ src/client/page-components/RecipePage/RecipePage.tsx
@@ -11,6 +11,28 @@ interface RecipePageProps {
     recipe: Recipe
 }
 
+const renderPreviewImageTag = (recipe: Recipe) => {
+    if (recipe.fbPreviewImageUrl) {
+        return (
+            <>
+                <meta property="og:image" content={recipe.fbPreviewImageUrl} />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="600" />
+            </>
+        )
+    } else if (recipe.imageUrl) {
+        return (
+            <>
+                <meta property="og:image" content={recipe.imageUrl} />
+                <meta property="og:image:width" content="300" />
+                <meta property="og:image:height" content="300" />
+            </>
+        )
+    } else {
+        return null
+    }
+}
+
 const RecipePage: React.FC<RecipePageProps> = ({ recipe }) => {
     const [isDesktop, setDesktop] = useState(false)
 
@@ -23,13 +45,14 @@ const RecipePage: React.FC<RecipePageProps> = ({ recipe }) => {
     return (
         <>
             <Head>
-                <title>mumsmums - {recipe.name}</title>
-                <meta property="og:title" content={recipe.name} key="title" />
+                <title>{`mumsmums - ${recipe.name}`}</title>
+                <meta property="og:title" content={`mumsmums - ${recipe.name}`} key="title" />
                 <meta property="og:site_name" content="mumsmums" />
                 <meta property="og:type" content="article" />
                 <meta property="og:url" content={`https://mumsmums.app/recipe/${recipe.recipeId}`} />
                 <meta property="og:locale" content="sv_SE" />
-                {recipe.imageUrl && <meta property="og:image" content={recipe.imageUrl} />}
+                {renderPreviewImageTag(recipe)}
+                {recipe.description && <meta property="og:description" content={recipe.description} />}
             </Head>
             {isDesktop && <RecipeDesktop recipe={recipe} />}
             {!isDesktop && <RecipeMobile recipe={recipe} />}

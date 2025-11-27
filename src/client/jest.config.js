b371fd74c1beaa4f0689f260c34b2bf1d6877903
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2025-11-27 15:27:50 +0100

    Add initial jest test for frontend

---
+++ src/client/jest.config.js
@@ -0,0 +1,22 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // The path to the Next.js app to load next.config.js and .env files in the test environment
+  dir: './',
+})
+
+// Any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+}
+
+// Export to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)

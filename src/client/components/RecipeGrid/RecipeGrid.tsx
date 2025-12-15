5d187294af5229e01a71b596fc01ad816cad9dbc
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2025-12-15 15:35:23 +0100

    Move gql consts to separate queries file

--- src/client/components/RecipeGrid/RecipeGrid.tsx
+++ src/client/components/RecipeGrid/RecipeGrid.tsx
@@ -1,23 +1,13 @@
 import React from 'react'
 import { useQuery } from '@apollo/client/react'
-import { gql } from '@apollo/client'
 import styles from './RecipeGrid.module.css'
 import { GetRecipesQueryResult } from '../../graphql/types'
+import { GET_RECIPE_PREVIEWS } from '../../graphql/queries'
 
 import ImageGrid from '../ImageGrid/ImageGrid'
 
-const GET_RECIPES = gql`
-  query {
-    recipes {
-      recipeId,
-      name,
-      imageUrl
-    }
-  }
-`
-
 const RecipeList = () => {
-    const { loading, error, data } = useQuery<GetRecipesQueryResult>(GET_RECIPES)
+    const { loading, error, data } = useQuery<GetRecipesQueryResult>(GET_RECIPE_PREVIEWS)
 
     if (loading) return null
     if (error) return <p>Error: {error.message}</p>

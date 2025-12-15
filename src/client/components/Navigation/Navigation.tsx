8da4c112b2ae84527c585c3b05800878c938bde0
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2025-12-15 14:56:01 +0100

    Add skeleton for search panel

--- src/client/components/Navigation/Navigation.tsx
+++ src/client/components/Navigation/Navigation.tsx
@@ -1,43 +1,62 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import Link from 'next/link'
 
 import { FEATURE_FLAGS } from '../../constants/featureFlags'
 import Icon from '../Icon/Icon'
+import SearchPanel from '../SearchPanel/SearchPanel'
 import style from './Navigation.module.css'
 
 const Navigation = () => {
+    const [isSearchOpen, setIsSearchOpen] = useState(false)
+    const searchButtonRef = useRef<HTMLButtonElement>(null)
+
     return (
-        <div className={style.header}>
-            <nav className={style.nav}>
+        <>
+            <div className={style.header}>
+                <nav className={style.nav}>
+                    <div className={style.navLeft}>
+                        {FEATURE_FLAGS.MENU && (
+                            <button className={style.iconButton} aria-label="Meny">
+                                <Icon name="menu-burger" size={26} />
+                            </button>
+                        )}
+                    </div>
 
-                <div className={style.navLeft}>
-                    {FEATURE_FLAGS.MENU && (
-                        <button className={style.iconButton} aria-label="Meny">
-                            <Icon name="menu-burger" size={26} />
-                        </button>
-                    )}
-                </div>
+                    <div className={style.navCenter}>
+                        <Link href="/" className={style.home} aria-label="Hem">
+                            <Icon name="home" size={28} />
+                        </Link>
+                    </div>
 
-                <div className={style.navCenter}>
-                    <Link href="/" className={style.home} aria-label="Hem">
-                        <Icon name="home" size={28} />
-                    </Link>
-                </div>
+                    <div className={style.navRight}>
+                        {FEATURE_FLAGS.SEARCH && (
+                            <button
+                                ref={searchButtonRef}
+                                className={style.iconButton}
+                                aria-label="Sök"
+                                onClick={() => setIsSearchOpen(!isSearchOpen)}
+                            >
+                                <Icon name="pan-frying" size={26} />
+                            </button>
+                        )}
+                        {FEATURE_FLAGS.LOGIN && (
+                            <button className={style.iconButton} aria-label="Logga in">
+                                <Icon name="circle-user" size={26} />
+                            </button>
+                        )}
+                    </div>
+                </nav>
+            </div>
 
-                <div className={style.navRight}>
-                    {FEATURE_FLAGS.SEARCH && (
-                        <button className={style.iconButton} aria-label="Sök">
-                            <Icon name="pan-frying" size={26} />
-                        </button>
-                    )}
-                    {FEATURE_FLAGS.LOGIN && (
-                        <button className={style.iconButton} aria-label="Logga in">
-                            <Icon name="circle-user" size={26} />
-                        </button>
-                    )}
-                </div>
-            </nav>
-        </div>
+            {/* Search Panel */}
+            {FEATURE_FLAGS.SEARCH && (
+                <SearchPanel
+                    isOpen={isSearchOpen}
+                    onClose={() => setIsSearchOpen(false)}
+                    triggerRef={searchButtonRef}
+                />
+            )}
+        </>
     )
 }
 
